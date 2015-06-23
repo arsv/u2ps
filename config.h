fbab9faa47275c5a11183d98130b72daafe2dcb0
Alex Suykov <alex.suykov@gmail.com>
2015-06-23 22:40:53 +0300

    comments

--- config.h
+++ config.h
@@ -1,3 +1,9 @@
+/* Default Ghostscript command, used to run font/resource reduction scripts */
 #define GS "gs"
+
+/* Path to psfrem executable, used in u2ps */
 #define PATH "."
+
+/* Path to postscript library directory; psfrem and ttf2pt42 will use -I$BASE
+   when running $GS above */
 #define BASE "ps"
